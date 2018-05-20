d2602d21aa3b7eaeb1d528791e7c5f9f78f6a5b6
austinirvine <aa1irvine@gmail.com>
2018-05-20 17:34:13 -0500

    Fix: Small bug of routing to sink obj

--- project/WorldGenerator.gd
+++ project/WorldGenerator.gd
@@ -5,8 +5,8 @@ var worldObjects = [];
 var tileObject = load("res://Tile.tscn");
 var constructLib = load("res://items/Construction_MeshLibrary.meshlib");
 
-var toilet = load("res://items/plumbing/Toilet/Toilet.tscn");
-var sink = load("res://items/plumbing/Toilet/Toilet.tscn");
+var toiletObj = load("res://items/plumbing/Toilet/Toilet.tscn");
+var sinkObj = load("res://items/plumbing/Sink/Sink.tscn");
 
 var objContainer = [];
 var currentSelection;
